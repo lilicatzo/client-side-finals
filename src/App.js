c9963f602f78497ba0942096389a2a764ea649cc
LIDYA AURELCIA <lidyaaurelcia@your-email.com>
2023-12-20 14:13:29 +0700

    Added mealsearch component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MealSearch from './components/MealSearch';
+fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error fetching meal details:', error));
 
 function App() {
-  const [view, setView] = useState('search');
+  const [view, setView] = React.useState('search');
 
   const switchView = (newView) => {
     setView(newView);
@@ -10,12 +14,12 @@ function App() {
 
   return (
     <div className="App">
-      {view === 'search' && <MealSearch switchView={switchView} />}
+      <h1>Meal Search Platform</h1>
+      <MealSearch switchView={switchView} />
     </div>
   );
 }
 
 export default App;
-
 
 
--- src/components/MealSearch.js
+++ src/components/MealSearch.js
@@ -3,36 +3,80 @@ import React, { useState, useEffect } from 'react';
 const MealSearch = ({ switchView }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedMeal, setSelectedMeal] = useState(null);
 
   const searchMeal = async () => {
     try {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
       const data = await response.json();
-      setSearchResults(data.meals || []); 
+      setSearchResults(data.meals || []);
+      setSelectedMeal(null);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  useEffect(() => {
-    if (searchTerm) {
-      searchMeal();
+  const getRecipe = async (mealId) => {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+      const data = await response.json();
+      setSelectedMeal(data.meals[0]);
+    } catch (error) {
+      console.error('Error fetching recipe:', error);
     }
-  }, [searchTerm]);
+  };
 
   return (
     <div>
-      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-      <button onClick={() => searchMeal()}>Search</button>
-      <div>
-        {searchResults.map((meal) => (
-          <div key={meal.idMeal}>
-            <h3>{meal.strMeal}</h3>
-            {}
+      {selectedMeal ? (
+        <div>
+          <h2>{selectedMeal.strMeal}</h2>
+          <img
+            src={
+                selectedMeal.strMealThumb.startsWith('http')
+                ? selectedMeal.strMealThumb
+                : `https://www.themealdb.com/images/media/meals/${selectedMeal.strMealThumb}/preview`
+            }
+            alt={selectedMeal.strMeal}
+            />
+          <h3>Ingredients:</h3>
+          <ul>
+            {Array.from({ length: 20 }, (_, i) => i + 1)
+              .filter((i) => selectedMeal[`strIngredient${i}`])
+              .map((i) => (
+                <li key={i}>
+                  {selectedMeal[`strMeasure${i}`]} {selectedMeal[`strIngredient${i}`]}
+                </li>
+              ))}
+          </ul>
+          <h3>Recipe:</h3>
+          <p>{selectedMeal.strInstructions}</p>
+          <button onClick={() => setSelectedMeal(null)}>Back to Search</button>
+        </div>
+      ) : (
+        <div>
+          <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          <button onClick={() => searchMeal()}>Search</button>
+          <div>
+          {searchResults.map((meal) => (
+  <div key={meal.idMeal} onClick={() => getRecipe(meal.idMeal)}>
+    <h3>{meal.strMeal}</h3>
+    <img
+      src={
+        meal.strMealThumb.startsWith('http')
+          ? meal.strMealThumb
+          : `https://www.themealdb.com/images/media/meals/${meal.strMealThumb}/preview`
+      }
+      alt={meal.strMeal}
+    />
+  </div>
+))}
+
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
+
   );
 };
 
